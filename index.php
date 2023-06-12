e6c4e53bce55d308824b54cc79550f7341c8004a
RAKOTOARISON Tiavina Gael <kelydoda724@gmail.com>
2023-06-12 15:54:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php
+    include_once("inc/fonction.php");
     $regions = array("Analamanga", "Atsinanana", "Alaotra-Mangoro", "Amoron'i Mania", "Analanjirofo", "Androy", "Anosy", "Atsimo-Andrefana", "Atsimo-Atsinanana", "Atsimo-Atsiny", "Betsiboka", "Boeny", "Bongolava", "Diana", "Haute-Matsiatra", "Ihorombe", "Itasy", "Melaky", "Menabe", "Sava", "Sofia", "Vakinankaratra", "Vatovavy-Fitovinany");
+    $provinces = getAllProvince();
     $groups = array_chunk($regions, 5);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Regions of Madagascar</title>
+  <title>Provinces of Madagascar</title>
   <link rel="stylesheet" type="text/css" href="./assets/css/index.css">
 </head>
 <body>
-  <h1>Madagascar` s Region</h1>
+  <h1>Madagascar` s Provinces</h1>
 
   <?php 
   foreach($groups as $group){ 
   ?>
 
     <ul>
-      <?php foreach($group as $region){ ?>
-        <li><?= $region ?></li>
+      <?php foreach($provinces as $province){ ?>
+        <li><?= $province['nom'] ?></li>
       <?php } ?>
     </ul>
 
